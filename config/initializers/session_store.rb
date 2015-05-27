17a5fc879eba34a698dec868e9fbe9b02f0dab22
Stephen Mariano Cabrera <stephen.m.cabrera@gmail.com>
2015-05-27 16:24:17 -0400

    Fix the names to refer to the correct name of the app

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_blocipedia_session'
+Rails.application.config.session_store :cookie_store, key: '_code-camino_session'
